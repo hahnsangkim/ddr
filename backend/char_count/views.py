dac8d36636452fe407c5b520f82384c1850e6199
Hahnsang Kim <hkim@inspro9.com>
2020-02-02 06:27:07 -0800

    feat: chat count

---
+++ backend/char_count/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import JsonResponse
+
+
+def char_count(request):
+    text = request.GET.get("text", "")
+
+    return JsonResponse({"count": len(text)})
--- backend/hello_world/settings.py
+++ backend/hello_world/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'char_count.apps.CharCountConfig'
 ]
 
 MIDDLEWARE = [
